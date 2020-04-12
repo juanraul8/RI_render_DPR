3e8aa2064f3a0a9456b273c417cab2706d3b6bcb
Juan Raúl <juanraul.padron@gmail.com>
2020-04-12 21:00:57 +0200

    Running Step 2

--- step_2/my_render.py
+++ step_2/my_render.py
@@ -7,10 +7,13 @@ import os
 import sys
 sys.path.append('../utils/cython')
 sys.path.append('../utils')
+
 import numpy as np
+from utils.cython.mesh_core_cython import *
 import mesh_core_cython
 import cv2
 from load_texture import *
+#from utils.load_texture import *
 import time
 
 
